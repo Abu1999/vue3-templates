02e8ef3cb20dc87b50082725cd549cf0dc0ae252
bbw <1526699702@qq.com>
2023-05-30 11:20:37 +0800

    ci(hooks): 新增axios封装

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src') // 路径别名
     }
   },
   server: {
