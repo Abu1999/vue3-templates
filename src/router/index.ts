70e495ebb4f559d2d40dbc4fb6076d329e604361
bbw <1526699702@qq.com>
2023-07-11 12:00:50 +0800

    fix(deploy): 修复部署问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import NProgress from 'nprogress'
 import { createRouter, createWebHistory } from 'vue-router'
+import { defineAsyncComponent } from 'vue';
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,7 +12,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: defineAsyncComponent(() => import('../views/AboutView.vue'))
     },
     {
       path: '/app1',
@@ -19,7 +20,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/app1.vue')
+      component: defineAsyncComponent(() => import('../views/app1.vue'))
     },
   ]
 })
