b003a65b4868b1ff854b88679774a5ff5c053723
bbw <1526699702@qq.com>
2023-08-18 14:00:34 +0800

    fix: 修改页面生成配置

---
+++ auto-build-page/template/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <"%Class%" v-slot="{ state, commands }">
+
+  </"%Class%">
+</template>
+
+<script setup lang="ts">
+import "%Class%" from '@/models/"%class%"/"%class%".list'
+
+</script>
+
+<style scoped></style>
